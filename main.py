bab053bd335fe27aadc48f88c71793df5a3631d8
anna <125645248+annadoannn@users.noreply.github.com>
2023-06-18 00:26:35 -0700

    geared it more to llm, hashed out the base, and tested out the predict function

--- main.py
+++ main.py
@@ -3,59 +3,66 @@ from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 
 import os
-import openai
 
+#environmental variable?
 API_KEY = "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"
+os.environ["OPENAI_API_KEY"] = API_KEY
 
-openai.api_key = API_KEY
+llm = OpenAI(openai_api_key = "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")
+llm = OpenAI(temperature = 0.9)
 
-deadline = input(
-    "Welcome to MockTalk.ai! We're here to help you prepare for any upcoming interviews that you may have. \n"
-    "To start off, when is the date of your interview (MM/DD/YYYY)? \n")
-company = input("What is the company you are applying to: ")
-role = input("What is the role that you wish to apply as: ")
-difficulty = input("What is the experience level of the position: ")
+llm.predict("what would be a good app name for an interview chatbot?")
+ans = input(llm.predict("what would be a good app name for an interview chatbot?"))
+print(ans)
 
-interview_question = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Do not include both at the same time. Provide either a {difficulty} interview question or a leetcode prompt. You are an interviewing me who wants to work at {company} as a {difficulty} {role}.",
-    temperature=1,
-    max_tokens=150,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.0
-)
 
-answer = input(interview_question["choices"][0]["text"])
-
-while True:
-    question = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"How good was my response ({answer}) for an interview setting? How can I improve on my response?",
-        temperature=1,
-        max_tokens=150,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
-
-    input(question["choices"][0]["text"])
-
-    end = input("Are you done with answering questions for now? ")
-    if end.lower() == "yes":
-        print("/nThank you for trying MockTalk :) See you next time!")
-        break
-
-    else:
-        question = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"Ask a follow up interview question.",
-            temperature=1,
-            max_tokens=150,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0
-        )
-
-        answer = input(question["choices"][0]["text"])
-
+# deadline = input(
+#     "Welcome to MockTalk.ai! We're here to help you prepare for any upcoming interviews that you may have. \n"
+#     "To start off, when is the date of your interview (MM/DD/YYYY)? \n")
+# company = input("What is the company you are applying to: ")
+# role = input("What is the role that you wish to apply as: ")
+# difficulty = input("What is the experience level of the position: ")
+#
+# interview_question = openai.Completion.create(
+#     model="text-davinci-003",
+#     prompt=f"Do not include both at the same time. Provide either a {difficulty} interview question or a leetcode prompt. You are an interviewing me who wants to work at {company} as a {difficulty} {role}.",
+#     temperature=1,
+#     max_tokens=150,
+#     top_p=1.0,
+#     frequency_penalty=0.0,
+#     presence_penalty=0.0
+# )
+#
+# answer = input(interview_question["choices"][0]["text"])
+#
+# while True:
+#     question = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=f"How good was my response ({answer}) for an interview setting? How can I improve on my response?",
+#         temperature=1,
+#         max_tokens=150,
+#         top_p=1.0,
+#         frequency_penalty=0.0,
+#         presence_penalty=0.0
+#     )
+#
+#     input(question["choices"][0]["text"])
+#
+#     end = input("Are you done with answering questions for now? ")
+#     if end.lower() == "yes":
+#         print("/nThank you for trying MockTalk :) See you next time!")
+#         break
+#
+#     else:
+#         question = openai.Completion.create(
+#             model="text-davinci-003",
+#             prompt=f"Ask a follow up interview question.",
+#             temperature=1,
+#             max_tokens=150,
+#             top_p=1.0,
+#             frequency_penalty=0.0,
+#             presence_penalty=0.0
+#         )
+#
+#         answer = input(question["choices"][0]["text"])
+#
