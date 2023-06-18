3010ba07ea55f6cc8b0c2f6321f2033acf57a64b
anna <125645248+annadoannn@users.noreply.github.com>
2023-06-18 03:34:18 -0700

    hashed out old code, couldn't figure out openAI chat agent ;-;

--- main.py
+++ main.py
@@ -1,28 +1,92 @@
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import AgentType, initialize_agent, load_tools, Tool
+
+#chat model imports
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+#template imports
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+#for the agent
+from langchain import (
+    LLMMathChain,
+    OpenAI,
+    SerpAPIWrapper,
+    SQLDatabase,
+    SQLDatabaseChain,
+)
 
 import os
 
-#environmental variable?
+#environmental variable
 API_KEY = "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"
 os.environ["OPENAI_API_KEY"] = API_KEY
+os.environ["SERPAPI_API_KEY"] = "fcd27d3d2165e05a55c72e78206a097520eecee7c651fd3e5d88d3ef6b5992ce"
 
+chat = ChatOpenAI(temperature = 0.9)
 llm = OpenAI(openai_api_key = "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")
-llm = OpenAI(temperature = 0.9)
+llm = OpenAI(temperature = 0.9, model = "gpt-3.5-turbo-0613")
 
-llm.predict("what would be a good app name for an interview chatbot?")
-ans = input(llm.predict("what would be a good app name for an interview chatbot?"))
-print(ans)
-
-
-# deadline = input(
-#     "Welcome to MockTalk.ai! We're here to help you prepare for any upcoming interviews that you may have. \n"
-#     "To start off, when is the date of your interview (MM/DD/YYYY)? \n")
-# company = input("What is the company you are applying to: ")
-# role = input("What is the role that you wish to apply as: ")
-# difficulty = input("What is the experience level of the position: ")
+# load_tools(["serpapi", "llm-math"], llm=llm)
+# llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+# search = SerpAPIWrapper()
+# db = SQLDatabase.from_uri("sqlite:///chinook.db")
+# db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
 #
+# tools = [
+#     Tool(
+#         name="Search",
+#         func=search.run,
+#         description="useful for when you need to answer questions about current events. You should ask targeted questions",
+#     ),
+#     Tool(
+#         name="Calculator",
+#         func=llm_math_chain.run,
+#         description="useful for when you need to answer questions about math",
+#     ),
+#     Tool(
+#         name="FooBar-DB",
+#         func=db_chain.run,
+#         description="useful for when you need to answer questions about FooBar. Input should be in the form of a question containing full context",
+#     ),
+# ]
+# agent = initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS, verbose=True)
+#agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION
+#agent=AgentType.OPENAI_FUNCTIONS
+
+template = "You are an interview chatbot giving job seekers advice for an interview with {company} as {difficulty} {role} by {deadline}, by giving feedback and providing leet code or interview questions. "
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_template = "{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+#chat_prompt.format_messages(text = print("Welcome to MockTalk.ai! We're here to help you prepare for any upcoming interviews that you may have. \nTo start off:\n"), deadline = input("When is the date of your interview (MM/DD/YYYY)?"), company = input("What is the company you are applying to: "), role = input("What is the role that you wish to apply as: "), difficulty = input("What is the experience level of the position: "))
+chain = LLMChain(llm = chat, prompt = chat_prompt)
+chain.run(
+    text = print("Welcome to MockTalk.ai! We're here to help you prepare for any upcoming interviews that you may have. \nTo start off:\n"),
+    deadline = input("When is the date of your interview (MM/DD/YYYY)? "),
+    company = input("What is the company you are applying to: "),
+    role = input("What is the role that you wish to apply as: "),
+    difficulty = input("What is the experience level of the position: "),
+)
+
+# agent.run(
+#     print("Provide either a interview question based on difficulty or a leetcode prompt based on difficulty and . You are an interviewing me who wants to work at {company} as a {difficulty} {role}. Do not include both at the same time.")
+# )
+
+#"Then, you give feedback after their answer, and ask if they want to continue. Depending on their answer, you either stop the code when the user says no, or repeat this prompt when the user wants to continue."
+
 # interview_question = openai.Completion.create(
 #     model="text-davinci-003",
 #     prompt=f"Do not include both at the same time. Provide either a {difficulty} interview question or a leetcode prompt. You are an interviewing me who wants to work at {company} as a {difficulty} {role}.",
